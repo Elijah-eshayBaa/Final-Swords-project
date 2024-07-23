24d7f01a487ba89239455806bb2eb5985e8fac13
Elijah-eshayBaa <elile87@kingshigh.school.nz>
2024-07-23 15:05:05 +1200

    Version 1
    test

---
+++ Scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const speed = 100
+func _physics_process(delta):
+	player_movement(delta)
+
+
+func player_movement(delta):
+	
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("down"):
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("up"):
+		velocity.y = -speed
+		velocity.x = 0
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
+
